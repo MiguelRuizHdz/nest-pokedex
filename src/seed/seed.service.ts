a756e0c31a0de95b20ce88681cf2011f7b3970c2
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-16 21:51:37 -0500

    Insertar múltiples registros simultáneamente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -18,17 +18,37 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons;
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    
+    // const insertPromisesArray = [];
+    
+    const pokemonToInsert: { name: string, no: number }[] = [];
 
     data.results.forEach( async ({ name, url }) => {
       
       const segments = url.split('/');
       const no: number = +segments[ segments.length -2 ];
 
-      const pokemon = await this.pokemonModel.create({ name, no });
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      
+      // insertPromisesArray.push(
+      //   this.pokemonModel.create({ name, no })
+      // );
 
-      console.log({ name, no })
+      pokemonToInsert.push({ name, no }); // [{ name: bulbasaur, no: 1 }]
+
     });
+
+    // await Promise.all( insertPromisesArray );
+    
+    await this.pokemonModel.insertMany( pokemonToInsert );
+    // insert into pokemons (name, no)
+    // (name: bulbasaur, no: 1)
+    // (name: bulbasaur, no: 1)
+    // (name: bulbasaur, no: 1)
+    // (name: bulbasaur, no: 1)
 
     return 'Seed Executed';
   }
