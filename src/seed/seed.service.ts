343ba47934077b68d6e11d81f4bd496f1ef93422
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-09-17 20:12:36 -0500

    Crear un custom provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -5,22 +5,23 @@ import { Model } from 'mongoose';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { Pokemon } from '../pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 
 @Injectable()
 export class SeedService {
-
-  private readonly axios: AxiosInstance = axios;
   
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); // delete * from pokemons;
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
     
     // const insertPromisesArray = [];
     
